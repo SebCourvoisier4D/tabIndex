12a21d8b30994bbc280f5df137dd6a63221699a6
SebCourvoisier4D <sebastien.courvoisier@4d.com>
2015-05-12 14:10:48 +0200

    Fix the damn repo.

---
+++ tabIndex/Widgets/MyWidget/designer.js
@@ -0,0 +1,40 @@
+(function(MyWidget) {
+
+//    /* Default width and height of your widget */
+//    MyWidget.setWidth('200');
+//    MyWidget.setHeight('20');
+
+//    /* Define custom event for your widget */
+//    MyWidget.addEvent('myEvent');
+
+//    /* Customize existing properties */
+//    MyWidget.customizeProperty('test', {
+//        title: 'Test Static Value',
+//        description: 'Add a datasource to this property.'
+//    });
+
+//    /* Add a Label property */
+//    MyWidget.addLabel({
+//        'defaultValue': '',
+//        'position': 'top'
+//    });
+
+//    /* Set the Design and Styles panels */
+//    MyWidget.setPanelStyle({
+//        'fClass': true,
+//        'text': true,
+//        'background': true,
+//        'border': true,
+//        'sizePosition': true,
+//        'label': true,
+//        'disabled': ['border-radius']
+//    });
+
+//    /* Override widget's initialization */
+//    MyWidget.prototype.init = function() {
+//        this.node.innerHTML = "Widget Text"; /* Include text inside the widget */
+//    }
+
+});
+
+// For more information, refer to http://doc.wakanda.org/Wakanda/help/Title/en/page3870.html
